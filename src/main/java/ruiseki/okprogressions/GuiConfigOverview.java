9a53b04fe19f7a4f4057019cfa8366c49d3aee73
Shigure-Ruiseki <ruiseki0829@gmail.com>
2026-08-16 21:37:13 +0700

    ✨ feat(gui): Add config overview GUI

---
+++ src/main/java/ruiseki/okprogressions/GuiConfigOverview.java
@@ -0,0 +1,36 @@
+package ruiseki.okprogressions;
+
+import net.minecraft.client.gui.GuiScreen;
+
+import ruiseki.okcore.OKCore;
+import ruiseki.okcore.client.gui.config.ExtendedConfigGuiFactoryBase;
+import ruiseki.okcore.client.gui.config.GuiConfigOverviewBase;
+import ruiseki.okcore.init.ModBase;
+
+/**
+ * @author rubensworks
+ */
+public class GuiConfigOverview extends GuiConfigOverviewBase {
+
+    /**
+     * Make a new instance.
+     *
+     * @param parentScreen the parent GuiScreen object
+     */
+    public GuiConfigOverview(GuiScreen parentScreen) {
+        super(OKProgressions._instance, parentScreen);
+    }
+
+    @Override
+    public ModBase getMod() {
+        return OKProgressions._instance;
+    }
+
+    public static class ExtendedConfigGuiFactory extends ExtendedConfigGuiFactoryBase {
+
+        @Override
+        public Class<? extends GuiConfigOverviewBase> mainConfigGuiClass() {
+            return GuiConfigOverview.class;
+        }
+    }
+}
